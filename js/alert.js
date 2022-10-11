ece6374581719cf3e40e7e05fbe5938d856dab94
AGESILAS Ludovic <ldc45@outlook.fr>
2022-10-11 21:49:24 +0200

    restructuration

--- js/alert.js
+++ js/alert.js
@@ -12,3 +12,7 @@ setTimeout(function () {
 }
 
 cv.addEventListener('click', download)
+
+//https://stackoverflow.com/questions/55601867/how-to-change-section-on-scroll
+//https://www.youtube.com/watch?v=J1todp-4YOI&ab_channel=LeDesignerduWeb-%C3%89coleduWeb
+//https://www.mathieu-crevoulin.com/

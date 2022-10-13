c13f13353b6ad1d11ff908f336f1da4d12858c2b
AGESILAS Ludovic <ldc45@outlook.fr>
2022-10-13 16:34:22 +0200

    s

--- js/alert.js
+++ js/alert.js
@@ -13,6 +13,25 @@ setTimeout(function () {
 
 cv.addEventListener('click', download)
 
+
+let sections = document.getElementsByTagName('section');
+// tracks index of current section
+let currentSectionIndex = 0;
+
+document.addEventListener('wheel', e => {
+  if (e.wheelDeltaY > 0 && currentSectionIndex - 1 >= 0) {
+    // wheel up
+    sections[currentSectionIndex].className = '';
+    currentSectionIndex--;
+    sections[currentSectionIndex].className = 'active';
+  } else if (e.wheelDeltaY < 0 && currentSectionIndex + 1 < sections.length) {
+    // wheel down
+    sections[currentSectionIndex].className = '';
+    currentSectionIndex++;
+    sections[currentSectionIndex].className = 'active';
+  }
+});
+
 //https://stackoverflow.com/questions/55601867/how-to-change-section-on-scroll
 //https://www.youtube.com/watch?v=J1todp-4YOI&ab_channel=LeDesignerduWeb-%C3%89coleduWeb
 //https://www.mathieu-crevoulin.com/
